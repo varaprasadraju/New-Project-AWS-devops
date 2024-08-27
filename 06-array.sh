d8afc6544fc62c676d988e9e1abf9bc521743902
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-27 16:31:49 +0530

    03-varaibles.sh

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#index starts from zero, size is 4
+
+FRUITS=("Apple" "Kiwi" "Banana" "Orange")
+
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
+echo "Thirt fruit is ${FRUITS[2]}"
+echo "Fourth fruit is ${FRUITS[3]}"
+
+echo "All fruits is ${FRUITS[@]}"
