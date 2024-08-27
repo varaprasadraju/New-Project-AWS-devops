4e7d2b0f1af9a2156430affb120f8dd97e9fda7c
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-27 16:11:04 +0530

    03-varaibles.sh

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+
+read -s USERNAME #takes input in USERNAME variable
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your Password::"
+
+read -s Password
+
+echo "Password is: $Password"
