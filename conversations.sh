2d31da98b2fb0708e1919aa33b671423a87f5f87
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-27 10:36:35 +0530

    I have added new file conversations.sh

---
+++ conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hi Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine. How are you doing"
+echo "Suresh:: I am donig good Ramesh. What's going on?"
+echo "Ramesh:: I started learning shell scripting Suresh"
