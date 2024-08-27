1c51ae36e30e8ed6e7ca469f15096c5fd0bf3379
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-27 11:18:25 +0530

    03-varaibles.sh

--- 03-varaibles.sh
+++ 03-varaibles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 $PERSON1=Suresh
-$PERSON2=Ramesh
+$PERSON2=Prasad
 
-echo "$PERSON1:: Hi $PERSON2, How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine. How are you doing"
-echo "$PERSON1:: I am donig good $PERSON2. What's going on?"
-echo "$PERSON2:: I started learning shell scripting $PERSON1"
+echo "$PERSON1:: Hi {$PERSON2}, How are you?"
+echo "{$PERSON2}:: Hi $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am donig good {$PERSON2}. What's going on?"
+echo "{$PERSON2}:: I started learning shell scripting $PERSON1"
