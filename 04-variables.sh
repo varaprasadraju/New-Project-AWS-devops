1f3c52fb3de5b706e59f6ecc24bee1499c1eea50
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-27 15:56:36 +0530

    03-varaibles.sh

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am donig good ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learning shell scripting $PERSON1"
