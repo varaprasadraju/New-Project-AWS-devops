40a6b42c88b1efc78a8b80b1009b84eb4f630e56
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-27 16:14:20 +0530

    03-varaibles.sh

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,7 @@
 
 echo "Please enter your username::"
 
-read -s USERNAME #takes input in USERNAME variable
+read USERNAME #takes input in USERNAME variable
 
 echo "Username entered is: $USERNAME"
 
