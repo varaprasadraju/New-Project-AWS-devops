6105e3394e8668dc5ce7b7e77f013b78dcdf9a4d
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-28 16:31:36 +0530

    03-varaibles.sh

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -4,6 +4,6 @@ NUMBER=$1
 NUMBER=$2
 NUMBER=$3
 
-SUM=$(("$NUMBER1+$NUMBER2+$NUMBER3"))
+SUM=$(($NUMBER1+$NUMBER2+$NUMBER3))
 
-echo "Total of $NUMBER1, $NUMBER2 and $NUMBER3 is: $SUM"
+echo "Total of $NUMBER1 and $NUMBER2 and $NUMBER3 is: $SUM"
