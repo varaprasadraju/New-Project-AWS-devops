d86e2dda6c272fdd2ae7f111bfd904f8d862e4c9
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-28 19:07:16 +0530

    added new file

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,4 +4,4 @@ USERID=$(id -u) # # To run a command inside shell script --> $(command)
 
 # variable can be called in 2 ways --> $variable_name or ${variable_name}
 
-echo "Root user id is: $USERID
+echo "Root user id is: $USERID"
