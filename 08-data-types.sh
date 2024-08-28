dc90ab30e00c44b1c1191ef6a18974d092099d0e
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-28 16:56:51 +0530

    added new file

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 NUMBER1=$1  # need to pass values as arguments for the below variables while executing this script
-NUMBER2=$2
-NUMBER1=$3
+NUMBER2=$2  # enter the values while running the script
 
-SUM=$(($NUMBER1+$NUMBER2+$NUMBER3))
+SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total of $NUMBER1, $NUMBER2 and $NUMBER2 is: $SUM"
+echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
