b267943e2e547010741db84c746d3140750828cd
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-28 17:31:21 +0530

    added new file

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "All variables passed to the script: $@"
+
+echo "All variables passed to the script: $@"   # I want all the variables passed to the script.
+echo "Number of variables passed: $#"   # How many variables/args passed to the script.
+
