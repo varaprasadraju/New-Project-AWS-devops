a44a85b05fa01c6373bb4699d8bfa89f661b99ee
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-29 06:58:39 +0530

    added new file

--- 12-functions.sh
+++ 12-functions.sh
@@ -15,11 +15,11 @@ VALIDATE(){
     fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root priveleges"
-    exit 1
-fi
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script with root priveleges"
+#     exit 1
+# fi
 
 dnf list installed git
 
