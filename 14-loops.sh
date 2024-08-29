b16949da949fbd8feabca4a043cd2b4f8b92eea6
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-29 15:07:12 +0530

    added new file

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-for i  in {9..99}
+for i  in {9..19} #10 to 19
 
 do
     echo $i
 done
+
+echo "1"
+echo "2"
+echo "3"
+echo "4"
+echo "5"
+echo "6"
+echo "7"
+echo "8"
+echo "9"
+echo "10" 
