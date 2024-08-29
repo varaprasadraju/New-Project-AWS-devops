2399f50559b242477f1c69ccda34ff012832f304
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-29 15:08:10 +0530

    added new file

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -8,5 +8,5 @@ if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
 then
     echo "Given number: $NUMBER is greater than 20"
 else
-    echo "Given number: $NUMBER is less than 20"
+    echo "Given number: $NUMBER is less than or equal to 20"
 fi        
