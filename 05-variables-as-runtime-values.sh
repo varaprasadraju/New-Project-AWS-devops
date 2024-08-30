408393a66c4e70a5134b620ac927b556e3c821b0
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-30 13:49:14 +0530

    added new file

--- 05-variables-as-runtime-values.sh
+++ 05-variables-as-runtime-values.sh
@@ -3,14 +3,17 @@
 
 # variable can be called in 2 ways --> $variable_name or ${variable_name}
 
-echo "Please enter your username::"
+# echo "Please enter your username::"
+# read USERNAME #takes input in USERNAME variable
+# echo "Username entered is: $USERNAME"
+# echo "Please enter your Password::"
+# read -s Password
+# echo "Password is: $Password"
 
-read USERNAME #takes input in USERNAME variable
-
+echo "Please enter your Amazon username::"
+read -s USERNAME #takes input in USERNAME variable
 echo "Username entered is: $USERNAME"
-
-echo "Please enter your Password::"
-
+echo "Please enter your Amazon Password::"
 read -s Password
-
 echo "Password is: $Password"
+
