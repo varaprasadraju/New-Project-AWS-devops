a8b0900b8101d52ba7a4a4d7339529d7c27e485e
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-08-30 13:51:59 +0530

    added new file

--- 05-variables-as-runtime-values.sh
+++ 05-variables-as-runtime-values.sh
@@ -10,10 +10,17 @@
 # read -s Password
 # echo "Password is: $Password"
 
-echo "Please enter your Amazon username::"
-read -s USERNAME #takes input in USERNAME variable
-echo "Username entered is: $USERNAME"
-echo "Please enter your Amazon Password::"
-read -s Password
-echo "Password is: $Password"
+# echo "Please enter your Amazon username::"
+# read -s USERNAME #takes input in USERNAME variable
+# echo "Username entered is: $USERNAME"
+# echo "Please enter your Amazon Password::"
+# read -s Password
+# echo "Password is: $Password"
 
+
+# Read Password
+echo -n Password: 
+read -s password
+echo
+# Run Command
+echo $password
