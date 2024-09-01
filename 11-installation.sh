a56feda9128d0617f1328ae5512747111bc9bdb6
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-09-01 18:22:46 +0530

    added new file

--- 11-installation.sh
+++ 11-installation.sh
@@ -10,19 +10,19 @@ then
     exit 1
 fi
 
-dnf install gittttt -y
+dnf list installed git
 
 # #"echo $?" it will tell you the state of previous command/Exit status
 # #0 --> Success
 # #1-127 --> Failure
 
-# if [ $? -ne 0 ]
-# then
-#     echo "Git is not installed, going to install it"
-#     dnf install git -y
-# else
-#     echo "Git is already installed, nothing to do"
-# fi   
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it"
+    dnf install git -y
+else
+    echo "Git is already installed, nothing to do"
+fi   
 
 # #########################################################################
 
