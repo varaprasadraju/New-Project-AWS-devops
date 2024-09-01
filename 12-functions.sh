1b4f472051926112265a86ca78a90343011204d2
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-09-01 18:50:27 +0530

    added new file

--- 12-functions.sh
+++ 12-functions.sh
@@ -6,13 +6,7 @@ USERID=$(id -u) # # To run a command inside shell script --> $(command)
 #echo "Root user id is: $USERID"
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-        echo "$2 is: Fail"
-        exit 1
-    else
-        echo "$2 is: Success"
-    fi
+    echo "exit status $1"
 }
 
 if [ $USERID -ne 0 ]
@@ -20,31 +14,32 @@ then
     echo "Please run this script with root priveleges"
     exit 1
 fi
+VALIDATE $?
 
-dnf list installed git
+# dnf list installed git
 
-#"echo $?" it will tell you the state of previous command
-#0 --> Success
-#1-127 --> Failure
+# #"echo $?" it will tell you the state of previous command
+# #0 --> Success
+# #1-127 --> Failure
 
-if [ $? -ne 0 ]
-then
-    echo "Git is not installed, going to install it"
-    dnf install git -y
-    VALIDATE $? "Installing Git"
-else
-    echo "Git is already installed, nothing to do"
-fi   
+# if [ $? -ne 0 ]
+# then
+#     echo "Git is not installed, going to install it"
+#     dnf install git -y
+#     VALIDATE $? "Installing Git"
+# else
+#     echo "Git is already installed, nothing to do"
+# fi   
 
-#########################################################################
+# #########################################################################
 
-dnf list installed mysql
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then
-    echo "mysql is not installed, going to install it"
-    dnf install mysql -y
-    VALIDATE $? "Installing Mysql"
-else
-    echo "mysql is already installed, nothing to do"
-fi 
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed, going to install it"
+#     dnf install mysql -y
+#     VALIDATE $? "Installing Mysql"
+# else
+#     echo "mysql is already installed, nothing to do"
+# fi 
