65293ad0ec4e09e306141a47f7fe8c3a70d8b5aa
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-09-01 18:04:03 +0530

    added new file

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,8 +5,8 @@ USERID=$(id -u) # # To run a command inside shell script --> $(command)
 #echo "Root user id is: $USERID"
 
 if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root priveleges"
+# then
+#     echo "Please run this script with root priveleges"
 #     exit 1
 # fi
 
