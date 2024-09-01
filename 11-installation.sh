7f80c26e9b0a52e711d053cc33606a660ec69334
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-09-01 18:03:03 +0530

    added new file

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 
 USERID=$(id -u) # # To run a command inside shell script --> $(command)
-
 #variable can be called in 2 ways --> $variable_name or ${variable_name}
-
 #echo "Root user id is: $USERID"
 
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root priveleges"
-    exit 1
-fi
+#     exit 1
+# fi
 
-dnf list installed git
+# dnf install git -y
 
-#"echo $?" it will tell you the state of previous command/Exit status
-#0 --> Success
-#1-127 --> Failure
+# #"echo $?" it will tell you the state of previous command/Exit status
+# #0 --> Success
+# #1-127 --> Failure
 
-if [ $? -ne 0 ]
-then
-    echo "Git is not installed, going to install it"
-    dnf install git -y
-else
-    echo "Git is already installed, nothing to do"
-fi   
+# if [ $? -ne 0 ]
+# then
+#     echo "Git is not installed, going to install it"
+#     dnf install git -y
+# else
+#     echo "Git is already installed, nothing to do"
+# fi   
 
-#########################################################################
+# #########################################################################
 
-dnf list installed mysql
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then
-    echo "mysql is not installed, going to install it"
-    dnf install mysql -y
-    if [ $? -ne 0 ]
-    then
-        echo "mysql installation is failure, please check"
-        exit 1
-    else
-        echo "mysql is success"
-    fi
-else
-    echo "mysql is already installed, nothing to do"
-fi 
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed, going to install it"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "mysql installation is failure, please check"
+#         exit 1
+#     else
+#         echo "mysql is success"
+#     fi
+# else
+#     echo "mysql is already installed, nothing to do"
+# fi 
