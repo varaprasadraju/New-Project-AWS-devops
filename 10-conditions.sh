72c6d406881ae7fe14d534c68c9aee1f3d4b91d4
esukapllyprasad <esukapally.prasad@aspiresys.com>
2024-09-01 16:50:25 +0530

    added new file

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,16 +5,16 @@ NUMBER=$2
 
 # variable can be called in 2 ways --> $variable_name or ${variable_name}
 
-if [ $NUMBER1 -gt 20 ] #gt, lt, eq, -ne, -ge, -le
+if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
 then
-    echo "Given number: $NUMBER1 is greater than 20"
+    echo "Given number: $NUMBER is greater than 20"
 else
-    echo "Given number: $NUMBER1 is less than or equal to 20"
+    echo "Given number: $NUMBER is less than or equal to 20"
 fi
 
-if [ $NUMBER2 -ge 10 ] #gt, lt, eq, -ne, -ge, -le
+if [ $NUMBER -ge 10 ] #gt, lt, eq, -ne, -ge, -le
 then
-    echo "Given number: $NUMBER2 is greater than 10"
+    echo "Given number: $NUMBER is greater than 10"
 else
-    echo "Given number: $NUMBER2 is less than or equal to 10"
+    echo "Given number: $NUMBER is less than or equal to 10"
 fi
